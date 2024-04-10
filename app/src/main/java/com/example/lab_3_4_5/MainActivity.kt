a2b2bbac5f051dfbb2227a99b5abae2a90adc9e5
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-04-11 00:34:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab_3_4_5/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lab_3_4_5
+
+import android.content.res.Resources
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private val Int.px: Int
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val headerView = findViewById<View>(R.id.some_id)
+    }
+}
