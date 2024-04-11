12ca0a963d5d21852b081fac108eef350182aacd
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-04-11 22:10:17 +0800

    Добавил тени :0

--- app/src/main/java/com/example/lab_3_4_5/PostsActivity.kt
+++ app/src/main/java/com/example/lab_3_4_5/PostsActivity.kt
@@ -1,6 +1,10 @@
 package com.example.lab_3_4_5
 
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,15 @@ class PostsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_posts)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.posts_activity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun likePost() {
+        val likePostImage = findViewById<ImageView>(R.id.like_post_0)
+        likePostImage.setImageResource(R.drawable.icon_heart_like_filled)
     }
 }
