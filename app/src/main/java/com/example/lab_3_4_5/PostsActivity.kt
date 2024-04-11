9fce720b21989a8bab321313b7ff84eef4d36ab2
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-04-11 11:40:08 +0800

    добавил активити для постов

--- app/src/main/java/com/example/lab_3_4_5/PostsActivity.kt
+++ app/src/main/java/com/example/lab_3_4_5/PostsActivity.kt
@@ -1,29 +1,20 @@
 package com.example.lab_3_4_5
 
-import android.content.Intent
-import android.content.res.Resources
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PostsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+        setContentView(R.layout.activity_posts)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun toSignUpActivity(view: View?) {
-        val intent = Intent(this, SignUpActivity::class.java)
-        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/lab_3_4_5/SignUpActivity.kt
+++ app/src/main/java/com/example/lab_3_4_5/SignUpActivity.kt
@@ -1,29 +1,27 @@
 package com.example.lab_3_4_5
 
 import android.content.Intent
-import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+        setContentView(R.layout.activity_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sign_up)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
 
-    fun toSignUpActivity(view: View?) {
-        val intent = Intent(this, SignUpActivity::class.java)
+    fun toLoginActivity(view: View?) {
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 }
